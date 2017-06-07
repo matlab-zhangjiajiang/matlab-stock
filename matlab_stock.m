c9bfef30e6b109d2cbad7baf3fa7128bfd1cdec3
wusezhangserver <393124085@qq.com>
2017-06-07 18:09:44 +0800

    变更

--- matlab_stock.m
+++ matlab_stock.m
@@ -1,2 +1,3 @@
-StockCode='002042';
-[StockInfo] = GetStockInfo_Web(StockCode);
+%%StockCode='002042';
+%%[StockInfo] = GetStockInfo_Web(StockCode); %%%获得指点股票代码的股票情况.
+[StockList,StockListFull] = GetStockList_Web;%%%获取A股市场全部股票名称和对应的代码
