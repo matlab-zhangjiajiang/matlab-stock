bd76e43139f36600b1faa9b8b35d1c7f1eb93c6f
wusezhangserver <393124085@qq.com>
2017-06-13 18:41:38 +0800

    注释

--- matlab_stock_pic_script.m
+++ matlab_stock_pic_script.m
@@ -1,5 +1,11 @@
 clear all;
 stockcode='600079';
 begindate='19970606';
-enddate ='20170609';
+enddate ='20170613';
 [stockdata,webdata] = GetStockTSDay_Web(stockcode,begindate,enddate);
+opendate = stockdata(:,1);
+closeprice = stockdata(:,5);
+
+xlable('交易时间点');
+ylable('收盘价');
+plot(opendate,closeprice,'r+:'); %%线形为点画线、颜色为红色、数据点标记为加号
