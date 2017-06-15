5434b6b25cc39e38141558f26a43a37744546e17
wusezhangserver <393124085@qq.com>
2017-06-15 19:04:36 +0800

    注释

--- matlab_stock.m
+++ matlab_stock.m
@@ -1,8 +1,10 @@
 clear all;
-StockCode='600079';
-[StockInfo] = GetStockInfo_Web(StockCode); %%%获得指点股票代码的股票情况.
+stockcode='600079';
+[StockInfo] = GetStockInfo_Web(stockcode); %%%获得指点股票代码的股票情况.
 [StockList,StockListFull] = GetStockList_Web;%%%获取A股市场全部股票名称和对应的代码
 stocklist = cell2mat(StockList(:,3));
 begindate='19970606';
 enddate ='20170609';
-[stockdata,webdata] = GetStockTSDay_Web(StockCode,begindate,enddate);
+[stockdata,webdata] = GetStockTSDay_Web(stockcode,begindate,enddate);  %%根据股票编码获取一段时间的日开盘情况
+
+
