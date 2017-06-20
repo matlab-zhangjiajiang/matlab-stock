5f0e12eb06f487f03b1d01dd215bc733794406ba
wusezhangserver <393124085@qq.com>
2017-06-20 13:37:43 +0800

    系统资源提交

--- get_all_stock_list_infor.m
+++ get_all_stock_list_infor.m
@@ -1,10 +1,10 @@
 clear all;
 clc;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%【保存所有股票交易信息】%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%[stocklist,stocklistfull] = GetStockList_Web; %%%获取所有的股票编码列表.
-%stockcodedouble =cell2mat(stocklist(:,3));  %%%获取相应的股票编码.
-%save('stocklist','stocklist');
-%[savelog,problist,newlist] = SaveStockTick(stocklist); %%保存所有的股票信息. 
+[stocklist,stocklistfull] = GetStockList_Web; %%%获取所有的股票编码列表.
+stockcodedouble =cell2mat(stocklist(:,3));  %%%获取相应的股票编码.
+save('stocklist','stocklist');
+[savelog,problist,newlist] = SaveStockTick(stocklist); %%保存所有的股票信息. 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%【保存股票分红配股信息】%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %stockcode ='000837' ; %%秦川机床
@@ -21,14 +21,14 @@ clc;
 %[savelog,prolist,newlist] = SaveStockFD(stocklist,1); %%%批量财务数据存储
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%【除权数据生成前后复权数据】%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-stockcode = '000837';  %%秦川机床
-str = ['股票编码为',stockcode];
-disp(str);
-begindate = '19980710';
-enddate ='20170619';
-[stockdatadouble,tsfactor] = GetStockTSDay_Web(stockcode,begindate,enddate);
-stockdata = stockdatadouble(:,1:end);
-scrsize = get(0,'ScreenSize');
+%stockcode = '000837';  %%秦川机床
+%str = ['股票编码为',stockcode];
+%disp(str);
+%begindate = '19980710';
+%enddate ='20170619';
+%[stockdatadouble,tsfactor] = GetStockTSDay_Web(stockcode,begindate,enddate);
+%stockdata = stockdatadouble(:,1:end);
+%scrsize = get(0,'ScreenSize');
 
 
 
